ab10ccc99f3ec03f3c7ccb9ef53267f26b41e22f
n ashok <ashokn1290@gmail.com>
2022-08-26 23:17:25 +0530

    ashok

---
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
@@ -0,0 +1,35 @@
+package com.samples.S01springcoredi;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext(
+				"com/samples/S01springcoredi/SpringConfig.xml");
+
+//       Employee emp = (Employee) springContainer.getBean("emp"); 
+//       System.out.println(emp);
+
+//       Employee em1 = new Employee();
+//       em1.setId(10);
+//       em1.setName("x");
+
+//       Hospital hospital = (Hospital) springContainer.getBean("hospital");
+//       System.out.println(hospital);
+
+//       CarDealer dealer = (CarDealer) springContainer.getBean("cardealer");
+//       System.out.println(dealer);
+
+//       Customer customer = (Customer) springContainer.getBean("customer");
+//       System.out.println(customer);
+
+//		ShoppingCart sc = (ShoppingCart) springContainer.getBean("shoppingcart");
+//		System.out.println(sc);
+
+		AbstractApplicationContext springContainer1 = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
+		Patient patient = (Patient) springContainer1.getBean("patient");
+		System.out.println(patient);
+		springContainer1.registerShutdownHook();
+	}
+}
